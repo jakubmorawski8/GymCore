100f682d2baa6653bbc3ff80663ed7b9e2f4473c
Arek <ari0987@o2.pl>
2021-04-19 23:09:26 +0200

    :white_check_mark: Base configuration of unit tests with examples

---
+++ GymCore.Application.UnitTests/Categories/Commands/CreateWorkoutEntityTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using GymCore.Application.Interfaces;
+using GymCore.Application.Profiles;
+using GymCore.Application.Requests.Workout.Commands.CreateWorkout;
+using GymCore.Application.UnitTests.Mocks;
+using Moq;
+using Shouldly;
+using Xunit;
+
+namespace GymCore.Application.UnitTests.Categories.Commands
+{
+    public class CreateWorkoutEntityTests
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<IWorkoutRepository> _mockWorkoutEntityRepository;
+
+        public CreateWorkoutEntityTests()
+        {
+            _mockWorkoutEntityRepository = RepositoryMock.GetWorkoutRepository();
+
+            var configurationProvider = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            });
+
+            _mapper = configurationProvider.CreateMapper();
+        }
+
+        [Fact]
+        public async Task Handle_ValidWorkout_AddedToWorkoutsRepository()
+        {
+            var handler = new CreateWorkoutCommandHandler(_mockWorkoutEntityRepository.Object, _mapper);
+
+            await handler.Handle(new CreateWorkoutCommand()
+            {
+                Name = "Test workout",
+                Description = "Test description",
+                Owner = "Test owner"
+            },
+            CancellationToken.None);
+
+            var allWorkouts = await _mockWorkoutEntityRepository.Object.ListAllAsync();
+            allWorkouts.Count.ShouldBe(3);
+        }
+    }
+}
---
+++ GymCore.Application.UnitTests/Mocks/RepositoryMock.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using GymCore.Application.Interfaces;
+using GymCore.Domain.Entities;
+using Moq;
+
+namespace GymCore.Application.UnitTests.Mocks
+{
+    public class RepositoryMock
+    {
+        public static Mock<IWorkoutRepository> GetWorkoutRepository()
+        {
+            var userEntity = new UserEntity();
+
+            var workoutEntities = new List<WorkoutEntity>()
+            {
+                new WorkoutEntity
+                {
+                    CreatedBy = userEntity,
+                    CreatedDate = DateTime.MinValue,
+                    Description = "Test description",
+                    ModifiedDate = DateTime.MinValue,
+                    Id = 1,
+                    Name = "Test name",
+                    UserWorkouts = new List<UserWorkoutEntity>()
+                },
+                 new WorkoutEntity
+                {
+                    CreatedBy = userEntity,
+                    CreatedDate = DateTime.MinValue,
+                    Description = "Test description",
+                    ModifiedDate = DateTime.MinValue,
+                    Id = 2,
+                    Name = "Test name",
+                    UserWorkouts = new List<UserWorkoutEntity>()
+                }
+            };
+
+            var mockWorkoutRepository = new Mock<IWorkoutRepository>();
+            mockWorkoutRepository.Setup(rep => rep.ListAllAsync()).ReturnsAsync(workoutEntities);
+
+            mockWorkoutRepository.Setup(rep => rep.AddAsync(It.IsAny<WorkoutEntity>())).ReturnsAsync((WorkoutEntity workoutEntity) =>
+            {
+                workoutEntities.Add(workoutEntity);
+                return workoutEntity;
+            });
+
+            return mockWorkoutRepository;
+        }
+    }
+}
