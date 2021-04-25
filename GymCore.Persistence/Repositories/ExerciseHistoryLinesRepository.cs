0b9e8b1511b15d8d62488c8d1fac0bc44f66db58
Arek <ari0987@o2.pl>
2021-04-25 18:36:31 +0200

    Base implementation of Repositories

---
+++ GymCore.Persistence/Repositories/ExerciseHistoryLinesRepository.cs
@@ -0,0 +1,12 @@
+﻿using GymCore.Application.Interfaces;
+using GymCore.Domain.Entities;
+
+namespace GymCore.Persistence.Repositories
+{
+    public class ExerciseHistoryLinesRepository : BaseRepository<ExerciseHistoryLinesEntity>, IExerciseHistoryLinesRepository
+    {
+        public ExerciseHistoryLinesRepository(GymCoreDBContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
---
+++ GymCore.Persistence/Repositories/WorkoutAreaRepository.cs
@@ -0,0 +1,12 @@
+﻿using GymCore.Application.Interfaces;
+using GymCore.Domain.Entities;
+
+namespace GymCore.Persistence.Repositories
+{
+    public class WorkoutAreaRepository : BaseRepository<WorkoutAreaEntity>, IWorkoutAreaRepository
+    {
+        public WorkoutAreaRepository(GymCoreDBContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
