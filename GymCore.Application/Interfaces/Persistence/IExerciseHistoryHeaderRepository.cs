e28eb3e16e2259724b3c40e19a6c1e9a0a6a2537
Jakub Morawski <jakub.morawski8@gmail.com>
2021-05-02 11:30:53 +0200

    changed folder for interfaces (persistence)

---
+++ GymCore.Application/Interfaces/Persistence/IExerciseHistoryHeaderRepository.cs
@@ -0,0 +1,8 @@
+﻿using GymCore.Domain.Entities;
+
+namespace GymCore.Application.Interfaces.Persistence
+{
+    public interface IExerciseHistoryHeaderRepository : IRepository<ExerciseHistoryHeaderEntity>
+    {
+    }
+}
---
+++ GymCore.Application/Interfaces/Persistence/IWorkoutRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using GymCore.Domain.Entities;
+
+namespace GymCore.Application.Interfaces.Persistence
+{
+    public interface IWorkoutRepository : IRepository<WorkoutEntity>
+    {
+        Task<bool> IsWorkoutNameUnitqueForUser(string workoutName, long userId);
+    }
+}
