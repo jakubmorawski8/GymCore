c7a60c1fc7ead136e84e98eda477d69dd443f6b2
Jakub Morawski <jakub.morawski8@gmail.com>
2021-05-03 15:46:07 +0200

    Fix issues

--- GymCore.Application/Interfaces/Persistence/IWorkoutRepository.cs
+++ GymCore.Application/Interfaces/Persistence/IWorkoutRepository.cs
@@ -1,10 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using GymCore.Domain.Entities;
 
 namespace GymCore.Application.Interfaces.Persistence
 {
     public interface IWorkoutRepository : IRepository<WorkoutEntity>
     {
-        Task<bool> IsWorkoutNameUnitqueForUser(string workoutName, long userId);
+        Task<bool> IsWorkoutNameUnitqueForUser(string workoutName, Guid userId);
     }
 }
