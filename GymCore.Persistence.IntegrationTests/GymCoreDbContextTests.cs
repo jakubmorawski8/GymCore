8a9789613c39bd375ebcf1420766525737aa0ea6
Arek <37308711+arkadiuszgrabowski@users.noreply.github.com>
2021-09-21 21:58:54 +0200

    :white_check_mark: Base configuration of persistence tests

---
+++ GymCore.Persistence.IntegrationTests/GymCoreDbContextTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using GymCore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Shouldly;
+using Xunit;
+
+namespace GymCore.Persistence.IntegrationTests
+{
+    public class GymCoreDbContextTests
+    {
+        private readonly GymCoreDbContext _gymCoreDbContext;
+
+        public GymCoreDbContextTests()
+        {
+            var dbContextOptions = new DbContextOptionsBuilder<GymCoreDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+
+            _gymCoreDbContext = new GymCoreDbContext(dbContextOptions);
+        }
+
+        [Fact]
+        public async void SaveEntityShouldGenerateId()
+        {
+            var exercise = new ExerciseEntity() { Name = "Test", Description = "Desc" };
+
+            _gymCoreDbContext.Exercise.Add(exercise);
+            await _gymCoreDbContext.SaveChangesAsync();
+
+            exercise.Id.ShouldBeOfType<Guid>();
+            exercise.Id.ToString().ShouldNotBeEmpty();
+        }
+    }
+}
