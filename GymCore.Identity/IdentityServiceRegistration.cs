67ebbd72679137fc3b724ef06a5f6e7aefde99db
jakubmorawski8 <34239665+jakubmorawski8@users.noreply.github.com>
2022-04-11 18:59:12 +0200

    fix migrations for snakeCaseNamingConvention

--- GymCore.Identity/IdentityServiceRegistration.cs
+++ GymCore.Identity/IdentityServiceRegistration.cs
@@ -10,7 +10,8 @@ namespace GymCore.Identity
         private static void AddDbContext<T>(this IServiceCollection services, string connectionString) where T : DbContext
         {
             services.AddDbContext<T>(options =>
-                options.UseNpgsql(connectionString,
+                options.UseSnakeCaseNamingConvention()
+                .UseNpgsql(connectionString,
                 x => x.MigrationsAssembly(typeof(T).Assembly.FullName)));
         }
 
--- GymCore.Identity/Migrations/20220411164130_init.Designer.cs
+++ GymCore.Identity/Migrations/20220411164130_init.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using GymCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace GymCore.Identity.Migrations
 {
     [DbContext(typeof(GymCoreIdentityDbContext))]
-    partial class GymCoreIdentityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220411164130_init")]
+    partial class init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
